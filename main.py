4bff328f88ddc37b996900c2e3497e620712925a
encer <encer.e@yandex.ru>
2025-07-05 16:38:11 +0300

    refactor(parser): move launch logic to separate file

--- main.py
+++ main.py
@@ -1,19 +1,11 @@
 import asyncio
 
-from app.infrastructure.managers import QueueManager, ProxyManager
-from app.infrastructure.storage import CoverManger
-from app.providers import MalProvider
-from app.providers.mal.parser import MalParser
 from app.core import *
 
 
 async def main():
-    # queue_manager = QueueManager(num_workers=10)
-    # await queue_manager.initialize()
-    # await queue_manager.run()
-    manager = ProxyManager()
-    await manager.initialize()
-    await manager.cleanup()
+    # Run fastapi app
+    pass
 
 
 if __name__ == "__main__":
