ae3a583d9aae62fec24f606de46d1ddadbf6d338
encer <encer.e@yandex.ru>
2025-07-10 22:36:08 +0300

    feat: implement FastAPI application

--- main.py
+++ main.py
@@ -1,14 +1,29 @@
-import asyncio
+from fastapi import FastAPI, HTTPException, Request
+from fastapi.responses import JSONResponse
+import uvicorn
 
 from app.core import *
 
+from app.api.v1 import routers
 
-async def main():
-    # Run fastapi app
-    pass
+
+app = FastAPI()
+app.include_router(routers.router)
+
+
+@app.exception_handler(HTTPException)
+async def http_exception_handler(_: Request, exc: HTTPException):
+    return JSONResponse(
+        status_code=exc.status_code,
+        content={
+            "status_code": exc.status_code,
+            "error": exc.__class__.__name__,
+            "message": exc.detail,
+        },
+    )
 
 
 if __name__ == "__main__":
     setup_logging()
 
-    asyncio.run(main())
+    uvicorn.run(app, log_config=None)
