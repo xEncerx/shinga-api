5a0032a150fe5db3a0b3538dc1675033ae0b457b
encer <encer.e@yandex.ru>
2025-07-12 23:09:35 +0300

    feat: add built-in exception handler for HTTP exceptions

---
+++ app/api/v1/utils.py
@@ -0,0 +1,20 @@
+from fastapi import Request, HTTPException
+from fastapi.responses import JSONResponse
+
+def exception_handler(_: Request, exc: HTTPException):
+    """
+    Built-in exception handler for HTTP exceptions.
+
+    Returns a JSON response with the following structure:
+    - Status code
+    - Error name
+    - Error detail
+    """
+    return JSONResponse(
+        status_code=exc.status_code,
+        content={
+            "status_code": exc.status_code,
+            "error": exc.__class__.__name__,
+            "detail": exc.detail,
+        },
+    )
