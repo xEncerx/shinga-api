711697a633e1eea640ae0e2c0963fe687b3ccb07
encer <encer.e@yandex.ru>
2025-07-12 23:13:12 +0300

    feat(api): add requests limiter

--- app/core/__init__.py
+++ app/core/__init__.py
@@ -1,5 +1,17 @@
+from slowapi.util import get_remote_address
+from slowapi import Limiter
+
+from redis.asyncio import Redis
+
 from .logging import setup_logging, logger
 from .config import Settings
 from .enums import *
 
 settings = Settings() # type: ignore
+limiter = Limiter(key_func=get_remote_address)
+redis = Redis(
+    host=settings.REDIS_HOST,
+    port=settings.REDIS_PORT,
+    db=0,
+    password=settings.REDIS_PASSWORD,
+)
--- main.py
+++ main.py
@@ -1,29 +1,36 @@
-from fastapi import FastAPI, HTTPException, Request
-from fastapi.responses import JSONResponse
+from slowapi import _rate_limit_exceeded_handler
+from slowapi.errors import RateLimitExceeded
+
+from fastapi.staticfiles import StaticFiles
+from fastapi import FastAPI, HTTPException
 import uvicorn
 
+from app.api import router, exception_handler
 from app.core import *
 
-from app.api.v1 import routers
+
+async def lifespan(app: FastAPI):
+    # Initialize any resources needed for the app
+    yield
+    # Cleanup resources when the app is shutting down
+    await redis.close()
 
 
 app = FastAPI()
-app.include_router(routers.router)
+app.include_router(router)
+app.state.limiter = limiter
+app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)  # type: ignore
+app.add_exception_handler(HTTPException, exception_handler)  # type: ignore
 
-
-@app.exception_handler(HTTPException)
-async def http_exception_handler(_: Request, exc: HTTPException):
-    return JSONResponse(
-        status_code=exc.status_code,
-        content={
-            "status_code": exc.status_code,
-            "error": exc.__class__.__name__,
-            "message": exc.detail,
-        },
-    )
+# Covers static files. Better to use nginx or other web server for production.
+app.mount(
+    "/media/covers", StaticFiles(directory=settings.COVER_STORAGE_PATH), name="covers"
+)
+app.mount(
+    "/media/avatars", StaticFiles(directory=settings.AVATAR_STORAGE_PATH), name="avatars"
+)
 
 
 if __name__ == "__main__":
     setup_logging()
-
     uvicorn.run(app, log_config=None)
