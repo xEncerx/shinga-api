a262812214f8dbb77d19953fc416b5c2b08b7c45
encer <encer.e@yandex.ru>
2025-09-18 16:05:46 +0300

    feat(api): add new endpoints for searching title by ID and getting recommendations

--- app/api/v1/routers/titles/search.py
+++ app/api/v1/routers/titles/search.py
@@ -1,3 +1,5 @@
+from fastapi_cache.decorator import cache
+
 from fastapi import APIRouter, Request
 
 from app.domain.use_cases import TitleSearchService, TitleSearchMode
@@ -24,4 +26,43 @@ async def global_search(
         params=search_fields,
         username=current_user.username,
         mode=TitleSearchMode.GLOBAL,
+    )
+
+
+@router.get("/{title_id}")
+@limiter.limit("60/minute")
+@cache(expire=60 * 30)
+async def get_title_data(
+    title_id: str,
+    *,
+    request: Request,
+) -> TitleSearchResponse:
+    """
+    Get detailed information about a specific title.
+
+    **Limits the request to 60 per minute.**
+    """
+    response = await TitleSearchService.get_title_by_id(title_id)
+    if not response:
+        raise TitleNotFound(detail=f"Title with ID: <{title_id}> not found.")
+
+    return TitleSearchResponse(content=[response])
+
+
+@router.get("/{title_id}/recommendations")
+@limiter.limit("60/minute")
+async def get_title_recommendations(
+    title_id: str,
+    *,
+    current_user: CurrentUserDep,
+    request: Request,
+) -> TitleSearchResponse:
+    """
+    Get recommendations for a specific title.
+
+    **Limits the request to 60 per minute.**
+    """
+    return await TitleSearchService.get_recommendations(
+        title_id,
+        username=current_user.username,
     )
