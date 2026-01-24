2f553f7af35503544080241928bd73dc56cf8a20
xEncerx <melixoff.da@yandex.ru>
2026-01-24 13:26:39 +0300

    refactor: organize use cases into content_aggregation module

--- src/application/use_cases/__init__.py
+++ src/application/use_cases/__init__.py
@@ -1,11 +1,1 @@
-from .process_cover_image import ProcessImageUseCase
-from .parse_source_page import ParseSourcePageUseCase
-from .consolidate_raw_title import ConsolidateRawTitleUseCase
-from .update_master_title import UpdateMasterTitleUseCase
-
-__all__ = [
-    "ProcessImageUseCase",
-    "ParseSourcePageUseCase",
-    "ConsolidateRawTitleUseCase",
-    "UpdateMasterTitleUseCase",
-]
+from .content_aggregation import *
