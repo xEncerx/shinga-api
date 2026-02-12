f51d4c364614e97a1c23c6e8583230cadee14a05
Encer <melixoff.da@yandex.ru>
2026-02-12 11:33:33 +0300

    refactor(broker): split broker into parsing and email brokers

--- src/infrastructure/tasks/__init__.py
+++ src/infrastructure/tasks/__init__.py
@@ -1,7 +1,5 @@
 from src.application.tasks import *
 
-from .scheduler import scheduler
-from .broker import broker
 from .dependencies import *
-
-__all__ = ["broker", "scheduler"]
+from .scheduler import *
+from .broker import *
