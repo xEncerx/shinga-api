7c5260a7e0767ab49964f701a70fe2754aeeace9
xEncerx <melixoff.da@yandex.ru>
2026-03-29 15:49:59 +0300

    fix: detach conflicting raw titles on mal_id collision

--- src/core/settings.py
+++ src/core/settings.py
@@ -18,7 +18,7 @@ class Settings(BaseSettings):
 
     # Application metadata
     APP_NAME: str = "Shinga"
-    APP_VERSION: str = "0.1.1"
+    APP_VERSION: str = "0.1.2"
 
     # Api settings
     API_URL: str = "http://localhost:8000"
